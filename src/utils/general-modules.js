4c5bb83e6c6e1de7c4a4d57058a4dd29a6e64d27
Vijay Pedagadi <vijay0996@proton.me>
2025-07-18 22:12:12 +0530

    try block on dict function

--- src/utils/general-modules.js
+++ src/utils/general-modules.js
@@ -6,21 +6,23 @@ import { GoogleGenAI } from '@google/genai';
 let ai;
 async function dict(message) {
     const word = message.content.split(' ')[1].toLowerCase();
-    let response = await fetch(`https://api.datamuse.com/words?sp=${word}&md=d&max=1`);
-    let data = await response.json();
-    if(data.length === 0) {
-        await message.reply("failed to get the definition. Check typos maybe");
-        return;
-    }
-    const definitions = data[0].defs
-        .map((def, i) => `${i + 1}. ${def.replace(/^[a-z]\t/, '')}`)
-        .join('\n');
-    const embed = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle(data[0].word)
-        .setDescription(definitions);
-    
-    await message.channel.send({ embeds: [embed] });
+    try {
+        let response = await fetch(`https://api.datamuse.com/words?sp=${word}&md=d&max=1`);
+        let data = await response.json();
+        if(data.length === 0) {
+            await message.reply("failed to get the definition. Check typos maybe");
+            return;
+        }
+        const definitions = data[0].defs
+            .map((def, i) => `${i + 1}. ${def.replace(/^[a-z]\t/, '')}`)
+            .join('\n');
+        const embed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle(data[0].word)
+            .setDescription(definitions);
+        
+        await message.channel.send({ embeds: [embed] });
+    } catch(e) { console.log("error", e); }    
 }
 
 async function instaDl(message, filePath, cookies) {
